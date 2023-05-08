b3c4d1376c87b6aaa2b2fa3b77df7565eb187e86
GloryCiel <58394480+GloryCiel@users.noreply.github.com>
2023-05-08 18:32:06 +0900

    [Silver II] Title: 스택 수열, Time: 56 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    stack<int> s;
+    vector<char> result;
+
+    int current = 1;
+    for (int i = 0; i < n; i++) 
+    {
+        int num;
+        cin >> num;
+
+        while (current <= num) 
+        {
+            s.push(current);
+            result.push_back('+');
+            current++;
+        }
+        if (s.top() == num) 
+        {
+            s.pop();
+            result.push_back('-');
+        } 
+        else 
+        {
+            cout << "NO" << endl;
+            return 0;
+        }
+    }
+
+    for (char op : result)
+    {
+        cout << op << '\n';
+    }
+
+    return 0;
+}
