ef44d9e0c8e9ddcd15628c8f91fa3c852c2bcb05
GloryCiel <58394480+GloryCiel@users.noreply.github.com>
2023-06-08 17:13:57 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;cin>>n;
+    vector<int> dp(n+1,0);
+    dp[1]=1;dp[2]=2;
+    for(int i=3;i<n+1;++i)
+    {
+        dp[i]=(dp[i-2]+dp[i-1])%10007;
+    }
+    cout<<dp[n];
+}
