fd04f15732c05247ad04e83a539ea3797952536d
GloryCiel <58394480+GloryCiel@users.noreply.github.com>
2023-06-12 15:18:00 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 88 ms, Memory: 6508 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+void dfs(vector<bool> &,vector<int>[],int);
+int connect = 0;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;cin>>N;
+    int M;cin>>M;
+
+    vector<bool> visited(N,false);
+    vector<int> graph[N];
+
+    for(int i=0;i<M;++i)
+    {
+        int tmp1,tmp2;cin>>tmp1>>tmp2;
+        tmp1-=1;tmp2-=1;
+        graph[tmp1].push_back(tmp2);
+        graph[tmp2].push_back(tmp1);
+    }
+    for(int i=0;i<N;++i)
+    {
+        if(!visited[i])
+        {
+            dfs(visited,graph,i);
+            connect++;
+        }
+    }
+    cout<<connect;
+}
+
+void dfs(vector<bool> &visited, vector<int> graph[],int cur)
+{
+    visited[cur]=true;
+    for(int i=0;i<graph[cur].size();++i)
+    {
+        int next = graph[cur][i];
+        if(!visited[next])
+        {
+            dfs(visited,graph,next);
+        }
+    }
+}
