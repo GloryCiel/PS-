d90e641d7ca8b488b70fe37ec913ac3f13b672df
GloryCiel <58394480+GloryCiel@users.noreply.github.com>
2024-04-02 16:23:24 +0900

    [Gold IV] Title: N-Queen, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n;cin>>n;
+    int sequence[]={1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596, 2279184, 14772512};
+
+    cout<<sequence[n-1];
+
+}
